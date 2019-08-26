c7fa1ffb0fb973b02ec7bf0fa30378ca7e1a6b37
Vic-Chang <s9826111@ulive.pccu.edu.tw>
2019-08-26 15:59:30 +0800

    Add exception notify and fix price slice wrong bug.

--- CoolpcMX500Notify.py
+++ CoolpcMX500Notify.py
@@ -4,42 +4,47 @@ import re
 import requests
 import sqlite3
 import datetime
+import traceback
 
 
-def CheckPrice():
-    # 取得分析價格
-    coolpc_url = 'https://www.coolpc.com.tw/evaluate.php'
-    cool = requests.get(coolpc_url)
-    soup = BeautifulSoup(cool.text, "lxml")  # 指定 lxml 作為解析器
+def check_price():
+    try:
+        # 取得分析價格
+        coolpc_url = 'https://www.coolpc.com.tw/evaluate.php'
+        cool = requests.get(coolpc_url)
+        soup = BeautifulSoup(cool.text, "lxml")  # 指定 lxml 作為解析器
 
-    # 兩種方式都可以
-    # mx_name = soup.find('option',text=re.compile('(?=.*(MX500))(?=.*(500G))',re.I)).text
-    mx_name = soup.find('option', text=lambda text: text and all(x in text for x in ['MX500', '500G'])).text
-    mx_price = int(mx_name[mx_name.rfind('$') + 1:mx_name.rfind(' ')])
+        # 兩種方式都可以
+        # mx_name = soup.find('option',text=re.compile('(?=.*(MX500))(?=.*(500G))',re.I)).text
+        mx_name = soup.find('option', text=lambda text: text and all(x in text for x in ['MX500', '500G'])).text
+        mx_name_split = asd[asd.rfind('$') + 1:]
+        mx_price = int(re.match(r'\d+', mx_name_split).group(0))
 
-    # 儲存資料
-    conn = sqlite3.connect('CoolpcMX500.db')
+        # 儲存資料
+        conn = sqlite3.connect('CoolpcMX500.db')
 
-    # 查詢db過往價格，若無資料則新增該筆資訊
-    mx500_data = conn.execute("SELECT * FROM MX500;").fetchall()
-    today = str(datetime.datetime.now().date())
-    if len(mx500_data) == 0:
-        sql = str.format('Insert into MX500 (ID,Logdate,Price) values(1,\'{0}\',{1})', today, mx_price)
-        conn.execute(sql)
-        conn.commit()
-        conn.close()
-    else:
-        oldprice = int(mx500_data[0][2])
-        olddate = mx500_data[0][1]
-        if oldprice != mx_price:
-            sql = str.format('Update MX500 set Logdate = \'{0}\', Price = {1} where ID=1', today, mx_price)
+        # 查詢db過往價格，若無資料則新增該筆資訊
+        mx500_data = conn.execute("SELECT * FROM MX500;").fetchall()
+        today = str(datetime.datetime.now().date())
+        if len(mx500_data) == 0:
+            sql = str.format('Insert into MX500 (ID,Logdate,Price) values(1,\'{0}\',{1})', today, mx_price)
             conn.execute(sql)
             conn.commit()
             conn.close()
-        SendNotify(oldprice, olddate, mx_price, today)
+        else:
+            oldprice = int(mx500_data[0][2])
+            olddate = mx500_data[0][1]
+            if oldprice != mx_price:
+                sql = str.format('Update MX500 set Logdate = \'{0}\', Price = {1} where ID=1', today, mx_price)
+                conn.execute(sql)
+                conn.commit()
+                conn.close()
+            send_notify(oldprice, olddate, mx_price)
+    except Exception as e:
+        exception_notify(str(e), traceback.format_exc())
 
 
-def SendNotify(oldprice, olddate, newprice, today):
+def send_notify(oldprice, olddate, newprice):
     wave = ''
     if (newprice - oldprice) > 0:
         wave = '+$' + str(newprice - oldprice)
@@ -56,5 +61,17 @@ def SendNotify(oldprice, olddate, newprice, today):
         requests.post(LineConfig.LINE_NOTIFY_URL, headers=headers, params=params)
 
 
+def exception_notify(error_msg, detail=''):
+    headers = {"Authorization": "Bearer " + LineConfig.LINE_NOTIFY_TOKEN}
+    params = {"message": 'Error ! 運行失敗，原因 : ' + error_msg + '\n詳細原因:\n' + detail}
+    requests.post(LineConfig.LINE_NOTIFY_URL, headers=headers, params=params)
+
+
+def exception_notify(error_msg):
+    headers = {"Authorization": "Bearer " + LineConfig.LINE_NOTIFY_TOKEN}
+    params = {"message": error_msg}
+    requests.post(LineConfig.LINE_NOTIFY_URL, headers=headers, params=params)
+
+
 if __name__ == '__main__':
-    CheckPrice()
+    check_price()
